0d8856f46b1bf30b70fd5084fd6a84668cf43776
IvanR <ivan.ramirezf@gmail.com>
2024-11-19 23:49:56 +1000

    Added isOpen state and render cards

--- src/AccordionItem.js
+++ src/AccordionItem.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="item">
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
